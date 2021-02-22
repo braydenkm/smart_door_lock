56b094d154535a96e09939a33c8c896b6c8bd3ba
Brayden Martin <braydenkaidmartin@gmail.com>
2021-02-22 13:02:31 -0600

    Initial commit

---
+++ smart_door_lock.ino
@@ -0,0 +1,33 @@
+/*
+CME/EE 495 - Capstone Design
+Bluetooth Smart Door Lock
+Created:      22.02.2021
+Last Updated: 22.02.2021 
+
+Group 14
+  Jackson Romanchuk - 11233901 - jwr920
+  Josh Bernier      - 11233918 - jdb145
+  Nick Anderson     - 
+  Brayden Martin    - 11232114 - bkm257
+*/
+
+// STRUCTS / ENUM
+enum State {
+  locked,
+  unlocked,
+  locking,
+  unlocking
+};
+
+// VARIABLES
+State lock_state = unlocked;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
